e8ccb94b01c4e407f69297f18c60bca9c9d1b238
cinthh <cinthia_zhang@163.com>
2025-04-16 10:13:28 +0800

    添加人脸认证

--- backend/FXTransaction/risk-service/src/main/java/com/hsbc/pws/risk/controller/AssessmentInfoController.java
+++ backend/FXTransaction/risk-service/src/main/java/com/hsbc/pws/risk/controller/AssessmentInfoController.java
@@ -2,14 +2,18 @@ package com.hsbc.pws.risk.controller;
 
 import com.hsbc.pws.common.response.Result;
 import com.hsbc.pws.risk.entity.AssessmentInfo;
+import com.hsbc.pws.risk.filter.FlaskFeign;
+import com.hsbc.pws.risk.filter.ThreadLocalUtil;
 import com.hsbc.pws.risk.pojo.AssessmentInfoCheck;
 import com.hsbc.pws.risk.service.AssessmentInfoService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 
@@ -28,7 +32,8 @@ import javax.annotation.Resource;
 public class AssessmentInfoController {
 	@Resource
 	private AssessmentInfoService assessmentInfoService;
-
+	@Resource
+	private FlaskFeign flaskFeign;
 	// 查询列表
 	@PostMapping("/list")
 	public Result<Page<AssessmentInfo>> list(@RequestBody AssessmentInfo request) throws Exception {
@@ -38,7 +43,7 @@ public class AssessmentInfoController {
 	public Result<List<AssessmentInfo>> listAll(@RequestBody AssessmentInfo request){
 		return Result.success(this.assessmentInfoService.findAll());
 	}
-	
+
 	// 保存
 	@PostMapping(value = { "/save" })
 	public Result<?> save(@Valid @RequestBody AssessmentInfo request)  throws Exception {
@@ -49,6 +54,36 @@ public class AssessmentInfoController {
 
 		this.assessmentInfoService.save(request);
 
+		return Result.success();
+	}
+
+	// 获取交易token
+	@PostMapping("getFaceToken")
+	Result<?> getTokenByFace(@RequestParam("image") MultipartFile file) {
+		return flaskFeign.getTokenByFace(file);
+	}
+
+	// token验证 -- KYC
+	@PostMapping("/kyc")
+	public Result<?> kyc(@RequestParam AssessmentInfo request, @RequestParam String faceToken) {
+		Map<String, Object> claims = ThreadLocalUtil.get();
+		String username = claims.get("username").toString();
+		String userId = claims.get("userId").toString();
+		try {
+			// 调用人脸 token 验证
+			boolean tokenResult = flaskFeign.validateFaceToken(username, faceToken);
+			if (!tokenResult) {
+				Result.error(401, "人脸Token验证失败：");
+			}
+			// 验证通过，保存/修改表单信息
+			if (request.getClientId().length() > 18) {
+				log.warn("客户编号长度错误! ClientId:{}", request.getClientId());
+				Result.error("客户编号长度错误!");
+			}
+			this.assessmentInfoService.save(request);
+		} catch (Exception e) {
+			Result.error(500, "充值异常：" + e.getMessage());
+		}
 		return Result.success();
 	}
 
@@ -79,3 +114,4 @@ public class AssessmentInfoController {
 		return Result.success(this.assessmentInfoService.findByClientId(clientId));
 	}
 }
+
